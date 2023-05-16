63b7e01690d6efdec4213f46957e7d84169d6da6
Alex Yun <alexyun04@gmail.com>
2023-05-16 15:00:47 -0500

    hw7

---
+++ HW7/p4-2.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+# table[i][j] = Q(s(i), a(j)). s(i) = {0 if A, 1 if B}, j = {1 if move, 0 if stay}
+table = np.zeros((2, 2))
+
+a = 0.5
+g = 0.8
+
+s_cur = 0
+iter = 200
+eps = 0.5
+
+gamma = a
+for i in range(iter):
+    action = np.argmax(table[s_cur])
+
+    if table[s_cur][0] == table[s_cur][1]:
+        # break tie
+        if np.random.rand(1) > 0.5:
+            action = 1
+
+    if 1-eps > np.random.rand(1):
+        # deviate from policy
+        action = 1 - action
+
+    r = 1 - action
+    s_next = s_cur
+    # change state if want move
+    if action == 1:
+        s_next = 1 - s_cur
+
+    table[s_cur][action] += a * (r + gamma * np.max(table[s_next]) - table[s_cur][action])
+    gamma *= g
+
+    s_cur = s_next
+
+print(table)
+
